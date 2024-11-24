3f5c556d6d85c4f17ed957d830535377ffbed533
littlecapa <littlecapa@googlemail.com>
2024-11-24 12:55:20 +0100

    renamed ches to chess

--- chess/wsgi.py
+++ chess/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ches project.
+WSGI config for chess project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ches.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chess.settings')
 
 application = get_wsgi_application()
